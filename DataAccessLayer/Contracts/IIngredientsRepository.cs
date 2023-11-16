6bdf91c94b7949404017268aedb48c6f9ea2e65d
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-11-16 04:14:14 +0100

    Startup code for this chapter

---
+++ DataAccessLayer/Contracts/IIngredientsRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Contracts
+{
+    public interface IIngredientsRepository
+    {
+        public void AddIngredient(Ingredient ingredient);
+        public List<Ingredient> GetIngredients(string? name = "");
+       
+    }
+}
